a9e51448b142a8f6f27ead962e25b25f2f73c5f9
ade5t <eryshev.u@gmail.com>
2019-04-09 13:58:55 +0300

    Дописал проверку строки на правильность

--- venv/recursive_descent.py
+++ venv/recursive_descent.py
@@ -4,6 +4,66 @@ import global_variables
 # скобках, так и без них. Изначально производится сворачивание
 # знаков "минус", а также замена унарного минуса на "!"
 
+# Функция проверки свернутой и закодированной инфиксной строки
+# на правильность
+# Вход - свернутая и закодированная инфиксная строка
+# Выход - строка с причиной ошибки или пустая строка
+def string_validation(str):
+    state = ""
+    prev_position = 0
+    tmp_num_brackets = 0
+    tmp_math_op = {'+', '-', '*', '/'}
+    if (not str[0] in {'(', '!'} and not str[0].isnumeric()):
+        return "Первый токен введен неверно"
+    if (str[len(str)-1] != ")" and not str[len(str)-1].isnumeric()):
+        return "Последний токен введен неверно"
+    elif(str[len(str)-1] == ")"):
+        tmp_num_brackets -= 1
+    state = get_token()
+    tmp_token = get_token()
+    while (tmp_token != None):
+        if (state == "!"):
+            if (tmp_token == "(" or isNum(tmp_token)):
+                prev_position = global_variables.index
+                state = tmp_token
+                tmp_token = get_token()
+            else:
+                return ' '.join(i.__str__() for i in ('После' ' - ' 'с позицей ', prev_position, ' стоит некорректный токен'))
+        elif (state == "("):
+            if (tmp_token == "(" or tmp_token == "!" or isNum(tmp_token)):
+                prev_position = global_variables.index
+                state = tmp_token
+                tmp_token = get_token()
+                tmp_num_brackets += 1
+            else:
+                return ' '.join(i.__str__() for i in ('После' ' ( ' 'с позицей ', prev_position, ' стоит некорректный токен'))
+        elif (state == ")"):
+            if (tmp_token == ")" or tmp_token in tmp_math_op):
+                prev_position = global_variables.index
+                state = tmp_token
+                tmp_token = get_token()
+                tmp_num_brackets -= 1
+            else:
+                return ' '.join(i.__str__() for i in ('После' ' ) ' 'с позицей ', prev_position, ' стоит некорректный токен'))
+        elif (state in tmp_math_op):
+            if (tmp_token == "(" or tmp_token == "!" or isNum(tmp_token)):
+                prev_position = global_variables.index
+                state = tmp_token
+                tmp_token = get_token()
+            else:
+                return ' '.join(i.__str__() for i in ('После', state, 'с позицей ', prev_position, ' стоит некорректный токен'))
+        elif (isNum(state)):
+            if (tmp_token in tmp_math_op or tmp_token == ")"):
+                prev_position = global_variables.index
+                state = tmp_token
+                tmp_token = get_token()
+            else:
+                return ' '.join(i.__str__() for i in ('После', state, 'с позицей ', prev_position, ' стоит некорректный токен'))
+        else:
+            return 'Неизвестная ошибка'
+    if (tmp_num_brackets != 0):
+        return "Количество '(' не соответсвует количеству ')'"
+
 # Функция, проверяющая является ли подстрока вещественным числом
 # Вход - подстрока, содержащая предположительно число
 # Выход - true - подстрока содержит вещественное число, иначе false
@@ -63,11 +123,10 @@ def folding(str):
         index += 1
     return str
 
-
 # Функция, которая заменяет унарный минус символом "!", чтобы унарный
 # и бинарный минус кодировались различным образом
 # У унарного минуса после сворачивания справа всегда число либо "(", а слева
-# всегда НЕ число
+# всегда НЕ число.
 # Вход - свернутая строка в инфиксной форме
 # Выход - свернутая строка в инфиксной форме с закодированным унарным минусом
 def unary_minus_coding(str):
@@ -149,20 +208,16 @@ def check_unary_minus():
         global_variables.postfix_string += tmp_token
 
 # Функция, проверяющая "(", ")"
-# Вход -
+# Вход -/
 # Выход -
 def check_brackets():
     if (global_variables.token == "("):
-        global_variables.num_brackets += 1
         global_variables.token = get_token()
         check_binary_plus_minus()
         if (global_variables.token != ")"):
-            print("В скобках дичь")
+            print("В скобках дичь рекурсия")
         else:
-            global_variables.num_brackets -= 1
             global_variables.token = get_token()
-            if (global_variables.num_brackets == 0 and global_variables.token == ")"):
-                print("В скобках дичь")
     else:
         check_number()
 
@@ -174,13 +229,34 @@ def check_number():
         global_variables.postfix_string += global_variables.token
         global_variables.token = get_token()
     else:
-        print("Введена дичь")
+        if (global_variables.token != "!" and global_variables.token != "(" and global_variables.index == len(global_variables.infix_string)):
+            print("В первом токене дичь рекурсия")
+        elif (len(global_variables.token) > 1):
+            print("В операнде дичь рекурсия")
 
 if __name__ == '__main__':
     #Временная дичь для проверки
     global_variables.input_string = input()
-    global_variables.folding_string = folding(global_variables.input_string)
-    global_variables.infix_string = unary_minus_coding(global_variables.folding_string)
-    global_variables.token = get_token()
-    check_binary_plus_minus()
-    print(global_variables.postfix_string)
+    if (global_variables.input_string == ""):
+        print("Строка пуста")
+    else:
+        global_variables.folding_string = folding(global_variables.input_string)
+        global_variables.infix_string = unary_minus_coding(global_variables.folding_string)
+        isOk = string_validation(global_variables.infix_string)
+        if ( isOk == ""):
+            global_variables.token = get_token()
+            check_binary_plus_minus()
+            global_variables.result_string = global_variables.postfix_string.replace("!", "-")
+            print(global_variables.postfix_string)
+        else:
+            print(isOk)
+
+    # Это реализация стека для следущего способа
+    # kek = []
+    # kek.append(0)
+    # kek.append(1)
+    # kek.append(2)
+    # print(kek)
+    # print("вытащил ", kek.pop())
+    # print(kek)
+    # print("вытащил ", kek.pop())
